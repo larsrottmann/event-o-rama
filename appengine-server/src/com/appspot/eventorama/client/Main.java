42583134d3f504913da43a1fd008c3bbc2b354c4
Lars Rottmann <lars.rottmann74@googlemail.com>
2011-06-09 11:33:19 +0200

    import of empty slim3 gwt project

---
+++ appengine-server/src/com/appspot/eventorama/client/Main.java
@@ -0,0 +1,11 @@
+package com.appspot.eventorama.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class Main implements EntryPoint {
+
+    public void onModuleLoad() {
+        // TODO Auto-generated method stub
+
+    }
+}
