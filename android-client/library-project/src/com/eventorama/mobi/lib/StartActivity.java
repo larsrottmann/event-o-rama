eadea0ede610030863f35706da8400b001431ba2
Dominik Helleberg <helleberg@gmx.de>
2011-06-28 22:43:45 +0200

    Added auto-forwarding and removed the activities from the backstack

--- android-client/library-project/src/com/eventorama/mobi/lib/StartActivity.java
+++ android-client/library-project/src/com/eventorama/mobi/lib/StartActivity.java
@@ -5,7 +5,9 @@ package com.eventorama.mobi.lib;
 import com.eventorama.mobi.lib.c2dm.C2DMReceiver;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -19,6 +21,30 @@ public class StartActivity extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        
+        //check preferences
+		SharedPreferences settings = getSharedPreferences(EventORamaApplication.PREFS_NAME, Context.MODE_PRIVATE);
+		String device_id = settings.getString(EventORamaApplication.PREFS_DEVICE_ID, "");
+		if(device_id.length() == 0)
+		{
+			//TODO: fire registration service
+		}
+		String username = settings.getString(EventORamaApplication.PREFS_USERNAME, "");
+		if(username.length() > 0)
+		{
+			//We are registered, go to EventStream
+			Intent i = new Intent();
+			i.setClass(getApplicationContext(), EventStreamActivity.class);
+			startActivity(i);
+		}
+		else
+		{
+			//We are not registered yet, go to Sign-In
+			Intent i = new Intent();
+			i.setClass(getApplicationContext(), SignUpActivity.class);
+			startActivity(i);
+		}
+        
         setContentView(R.layout.main);
         
         Button b = (Button) findViewById(R.id.button1);
