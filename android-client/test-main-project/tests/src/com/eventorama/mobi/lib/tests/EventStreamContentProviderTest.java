1759c249e4092046fcdccdda5c72df4a4f5fe6be
Dominik Helleberg <helleberg@gmx.de>
2011-06-28 14:16:36 +0200

    added tests for eventstream content provider

--- android-client/test-main-project/tests/src/com/eventorama/mobi/lib/tests/EventStreamContentProviderTest.java
+++ android-client/test-main-project/tests/src/com/eventorama/mobi/lib/tests/EventStreamContentProviderTest.java
@@ -42,24 +42,57 @@ public class EventStreamContentProviderTest extends ProviderTestCase3<EventStrea
     
     public void testInsertData()
     {
+    	final String TEST_TEXT = "hi text";
+    	
     	ContentProvider provider = getProvider();
     	
     	Uri uri = EventStreamContentProvider.content_uri;
-    	ContentValues values = new ContentValues();
-    	values.put(EventStreamContentProvider.Columns.TEXT, "hi text");
-    	Uri entryUri = provider.insert(uri, values);
+
+    	Cursor c = provider.query(uri, null, null, null, null);    	
+    	int entries = c.getCount();
+    	c.close();
+    	
+    	Uri entryUri = provider.insert(uri, null);
+    	c = provider.query(uri, null, null, null, null);
+    	assertEquals(entries+1, c.getCount());    
+    	
+    	ContentValues values = new ContentValues();    	
+		values.put(EventStreamContentProvider.Columns.TEXT, TEST_TEXT);
+		long now = System.currentTimeMillis(); 
+    	entryUri = provider.insert(uri, values);
     	
     	assertNotNull(entryUri);
     	
     	Log.v("TTEST", "got entry uri: "+entryUri);
     	//read it back
-    	Cursor c = provider.query(entryUri, null, null, null, null);
+    	c = provider.query(entryUri, null, null, null, null);
     	assertNotNull(c);
     	assertTrue(c.getCount() == 1);
     	
     	assertTrue(c.moveToFirst());
     	String text = c.getString(c.getColumnIndex(EventStreamContentProvider.Columns.TEXT));
-    	assertTrue(text.equals("hi text"));
+    	assertTrue(text.equals(TEST_TEXT));
+    	
+    	long created = c.getLong(c.getColumnIndex(EventStreamContentProvider.Columns.CREATED));    	
+    	assertTrue("created timstamp differs! is bigger then 100 ms "+(created-now)+ " created: "+created+" now "+now, new Long((created-now)).compareTo(new Long(100)) <= 0);
+    	assertTrue("created timstamp differs! less then 0 ms "+(created-now)+ " created: "+created+" now "+now, new Long((created-now)).compareTo(new Long(0)) >= 0);
+
+    }
+    
+    public void testUpdateData()
+    {
+    	final String TEST_TEXT = "hi text";
+
+    	ContentProvider provider = getProvider();   	
+    	Uri uri = EventStreamContentProvider.content_uri;
+    	
+    	Uri entryUri = provider.insert(uri, null);
+    	
+    	ContentValues cv = new ContentValues();
+    	cv.put(EventStreamContentProvider.Columns.TEXT, TEST_TEXT);
+    	
+    	int updates = provider.update(entryUri, cv, null, null);
+    	assertEquals(1, updates);
     	
     }
     
