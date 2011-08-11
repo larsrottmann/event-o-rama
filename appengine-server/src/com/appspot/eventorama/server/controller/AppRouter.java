e140f90110a1df4a516bbabdd72a5590f7174376
Lars Rottmann <lars.rottmann74@googlemail.com>
2011-08-11 14:09:11 +0200

    first draft of a simple user avatar store

--- appengine-server/src/com/appspot/eventorama/server/controller/AppRouter.java
+++ appengine-server/src/com/appspot/eventorama/server/controller/AppRouter.java
@@ -31,6 +31,10 @@ public class AppRouter extends RouterImpl {
         addRouting(
             "/app/{app_id}/users/{user_id}",
             "/users/users?app_id={app_id}&user_id={user_id}");
+        // User Profile Pic Management: POST / GET (single)
+        addRouting(
+            "/app/{app_id}/users/{user_id}/avatar",
+            "/avatar/avatar?app_id={app_id}&user_id={user_id}");
         
         // Activities Management: POST / GET (list)
         addRouting(
